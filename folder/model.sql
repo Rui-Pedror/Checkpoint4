44c6cb2b716869504389a28f022c6633eee7823e
rui <ruicoelho2001@hotmail.com>
2022-02-03 00:33:19 +0000

    wireframes, MCD

---
+++ folder/model.sql
@@ -0,0 +1,36 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+
+
+CREATE TABLE Relacao1 (
+
+)
+
+CREATE TABLE Relacao2 (
+
+)
+
+CREATE TABLE Entidade1 (
+id VARCHAR(10),
+framework VARCHAR(10),
+library VARCHAR(10)
+)
+
+CREATE TABLE User (
+id VARCHAR(10),
+firstName VARCHAR(10),
+lastName VARCHAR(10),
+email VARCHAR(10),
+location VARCHAR(10),
+idiom VARCHAR(10),
+Campo1 Número(4) PRIMARY KEY
+)
+
+CREATE TABLE Project (
+id VARCHAR(10),
+projecName VARCHAR(10),
+skill VARCHAR(10),
+tools VARCHAR(10)
+)
+
